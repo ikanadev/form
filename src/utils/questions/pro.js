b90353d8ed3c3c8ff55a53a9c75b5a67ad9d91a2
Kevv <taylorwebk@gmail.com>
2019-07-08 18:43:54 -0400

    primera pagina de profesionales

---
+++ src/utils/questions/pro.js
@@ -0,0 +1,72 @@
+export default ({
+  // ASPECTOS GENERALES
+  pro1: { title: 'Nombre y Ap.:' },
+  pro2: { title: 'Celular:' },
+  pro3: { title: 'Correo electrónico:' },
+  pro4: { title: 'Título que obtuvo:' },
+  pro5: { title: 'Gestión en que se tituló:' },
+  pro6: { title: 'Mención y Especialidad' },
+  pro7: { title: 'Género:', options: ['Mujer', 'Varon'] },
+  pro8: {
+    title: 'Estado civil:',
+    options: ['Soltero/a', 'Casado/a', 'Divorciado/a', 'Viudo/a']
+  },
+  pro9: {
+    title: 'Universidad donde se tituló',
+    options: ['UMSA', 'UMSS', 'UGRM', 'USFXCH', 'UAJMS', 'UATF', 'UTO', 'UAB', 'UAP', 'UNSXX', 'UPEA', 'UCB', 'EMI', 'UASB', 'UNIPOL', 'Otro']
+  },
+  pro10: {
+    title: 'Rango de edad:',
+    options: ['Menos de 25', '26-35', '36-45', '46-55', 'Más de 55']
+  },
+  pro11: { title: 'Diplomado(s):' },
+  pro12: { title: 'Especialidad(es):' },
+  pro13: { title: 'Maestría(s):' },
+  pro14: { title: 'Doctorado(s):' },
+  pro15: { title: 'Otro(s):' },
+  pro16: {
+    title: 'Certificaciones con las que cuenta:',
+    options: ['ORACLE', 'ISACA', 'SCRUM', 'COBIT', 'SAP', 'CISCO', 'AGILE', 'PMP', 'ITIL', 'Otro(s):']
+  },
+  pro17: { title: 'Mencione al menos 3 cursos que realizó como formación complementaria:' },
+  // SITUACIÓN LABORAL
+  pro18: {
+    title: 'Actualmente trabaja:',
+    options: ['A tiempo completo', 'A medio tiempo', 'De forma eventual', 'En emprendimiento propio', 'Como freelance', 'Otro:']
+  },
+  pro19: { title: 'Nombre de la institución:' },
+  pro20: { title: 'Año de ingreso a la institución:' },
+  pro21: { title: 'Prestaciones laborales:', options: ['Seguro de salud', 'Aportes para la jubilación', 'Otros:'] },
+  pro22: { title: 'Relación de su trabajo con la carrera:', options: ['Muy relacionado', 'Poco relacionado', 'No está relacionado'] },
+  pro23: {
+    title: 'Salario mensual en Bs.',
+    options: ['Menos de 3000', '3001-6000', '6001-9000', '9001-12000', '12001-15000', '15001-18000', '18001-21000', '21001-24000', 'Más de 24000']
+  },
+  pro24: { title: 'Alcance de la institución:', options: ['Local', 'Nacional', 'Internacional'] },
+  pro25: { title: 'Tipo de empleo:', options: ['Empleo de planta', 'Empleo eventual', 'Consultoría', 'Otros:'] },
+  pro26: { title: '' },
+  pro27: { title: '' },
+  pro28: { title: '' },
+  pro29: { title: '' },
+  pro30: { title: '' },
+  pro31: { title: '' },
+  pro32: { title: '' },
+  pro33: { title: '' },
+  pro34: { title: '' },
+  pro35: { title: '' },
+  pro36: { title: '' },
+  pro37: { title: '' },
+  pro38: { title: '' },
+  pro39: { title: '' },
+  pro40: { title: '' },
+  pro41: { title: '' },
+  pro42: { title: '' },
+  pro43: { title: '' },
+  pro44: { title: '' },
+  pro45: { title: '' },
+  pro46: { title: '' },
+  pro47: { title: '' },
+  pro48: { title: '' },
+  pro49: { title: '' },
+  pro50: { title: '' }
+})
